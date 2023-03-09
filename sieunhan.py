4eaa60cd1f2f8257f2bc857f3168d8e79d9f98ab
phamhoanghuy20001002 <huy.pho.60cntt@ntu.edu.vn>
2023-03-09 16:11:21 +0700

    init project

---
+++ sieunhan.py
@@ -0,0 +1,11 @@
+class sieunhan:
+	def __init__(self,pr_ten,pr_vk,pr_mau):
+		self.ten = pr_ten
+		self.vk = pr_vk
+		self.mau =pr_mau
+
+sieunhan_A = sieunhan("xanh","ca_map","xanh la")
+
+print("sieu nhan:",sieunhan_A.ten)
+print("sieu nhan:",sieunhan_A.vk)
+print("sieu nhan:",sieunhan_A.mau)
